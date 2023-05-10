c146030f5e5fbdfaa11d2bb7136961dcb2c0507f
Zaldef <78030935+Zaldef@users.noreply.github.com>
2023-05-09 21:26:12 -0300

    0.5

--- sistema_caixa/main.c
+++ sistema_caixa/main.c
@@ -1,18 +1,7 @@
-// 	╬ 206, ═ 205, ╠ 204, ╦ 203, ╩ 202, ╔ 201, ╚ 200, ╝ 188, ╗ 187, ║ 186, ╣ 185
-// Evitar colocar em tela cheia e depois minimizar
-    /*printf("Carregando");
-    sleep(1);
-    printf(".");
-    sleep(1);
-    printf(".");
-    sleep(1);
-    printf(".\n");*/
-    // tela inicial
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-#include <ncurses.h>
-/*#include <unistd.h> // função sleep */
+#include <unistd.h>
 void moldura_inicial(){
     printf("%c",201);
     for(int i=0; i<87; i++){
@@ -22,38 +11,45 @@ void moldura_inicial(){
     printf("%c",186);
     printf("                                     Bem vindo !!!                                     ");
     printf("%c\n",186);
-    printf("%c",204);
+    printf("%c",200);
     for(int i=0; i<87; i++){
         printf("%c",205);
     }
-    printf("%c\n",185);
+    printf("%c",188);
 }
-void verificacao_senha(int senha,int tent_senha,int qntd_tent_senha,int flag_senha){
+int verificacao_senha(int senha,int tent_senha,int qntd_tent_senha,int flag_senha){
     do{
-    printf("%c",186);
-    printf("Digite a sua senha para abrir o caixa:                                      ");
-    printf("%c",186);
-    gotoxy(10,5);
-    scanf("%d",&tent_senha);
-
-    if(tent_senha == senha){
-        flag_senha++;
-    }else{
-        qntd_tent_senha++;
+        if(qntd_tent_senha == 1){
+            printf("Senha incorreta, voce tem mais DUAS tentativas!!!");
+        }else if(qntd_tent_senha == 2){
+            printf("Senha incorreta, voce tem mais UMA tentativas!!!");
         }
-    }while(flag_senha == 0 || qntd_tent_senha>=3);
-    printf("%d",qntd_tent_senha);
-    printf("%d",flag_senha);
+        printf("\nDigite a sua senha para abrir o caixa: ");
+        scanf("%d",&tent_senha);
+        if(tent_senha == senha){
+            flag_senha++;
+            break;
+        }else{
+            qntd_tent_senha++;
+        }
+    }while(qntd_tent_senha<3);
+    if(qntd_tent_senha == 3){
+        printf("Sistema precisa ser reinicializado !!");
+        exit(1);
+    }else{
+        printf("Senha correta!!!\n");
+        printf("Abrindo caixa");
+        for(int i=0;i<3;i++){
+            sleep(1);
+            printf(".");
+        }
+        system("cls");
+    }
 }
 int main() {
     setlocale(LC_ALL, "pt_BR.UTF-8");
-    char ch = '*';
-    int x = 10, y = 5;
+
     moldura_inicial();
     verificacao_senha(2323,0,0,0);
-    move(x, y);
-    printf("%c\n",186);
-
-
     return 0;
 }
